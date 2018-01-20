171fa30f4e4c90c385ccc0e8864d9060135aa8f9
Leandro Baltazar <leandro.baltazar@cronn.de>
2018-01-21 00:27:22 +0100

    throw away liquibase

---
+++ src/main/java/ebaydb/controllers/TestController.java
@@ -0,0 +1,23 @@
+package ebaydb.controllers;
+
+import ebaydb.models.EbayItem;
+import ebaydb.repositories.EbayItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    private EbayItemRepository repo;
+
+    @RequestMapping("/items")
+    public Iterable<EbayItem> getAll() {
+        return repo.findAll();
+    }
+
+
+}
--- src/main/java/ebaydb/models/EbayItem.java
+++ src/main/java/ebaydb/models/EbayItem.java
@@ -1,11 +1,9 @@
 package ebaydb.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "ebay_items")
 public class EbayItem {
 
     @Id
