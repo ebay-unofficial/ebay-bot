3e49f142c6eabd9cf319cb069ed88747796e0d19
Leandro Baltazar <leandro.baltazar@cronn.de>
2018-01-21 04:49:01 +0100

    add scheduled job

--- src/main/java/ebaydb/Application.java
+++ src/main/java/ebaydb/Application.java
@@ -2,9 +2,11 @@ package ebaydb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
